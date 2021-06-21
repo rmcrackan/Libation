5ec6994da766e92e1560d45e4e5bb24e586d7e08
Robert McRackan <rmcracka@itron.com>
2021-06-21 14:24:54 -0400

    Remove outdated proof of concept demo programs

--- AaxDecrypter/DecryptSupportLibraries.cs
+++ AaxDecrypter/DecryptSupportLibraries.cs
@@ -7,8 +7,6 @@ namespace AaxDecrypter
         // OTHER EXTERNAL DEPENDENCIES
         // ffprobe has these pre-req.s as I'm using it:
         // avcodec-58.dll, avdevice-58.dll, avfilter-7.dll, avformat-58.dll, avutil-56.dll, postproc-54.dll, swresample-3.dll, swscale-5.dll, taglib-sharp.dll
-        //
-        // something else needs the cygwin files (cyg*.dll)
 
         private static string appPath_ { get; } = Path.GetDirectoryName(Dinah.Core.Exe.FileLocationOnDisk);
         private static string decryptLib_ { get; } = Path.Combine(appPath_, "DecryptLib");
