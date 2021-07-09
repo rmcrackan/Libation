4bae07d36cf2fada2ade0cc19b3e7909be3d2498
Robert McRackan <rmcracka@itron.com>
2021-07-08 20:43:14 -0400

    debugging added

--- LibationWinForms/Dialogs/Login/MfaDialog.cs
+++ LibationWinForms/Dialogs/Login/MfaDialog.cs
@@ -12,9 +12,13 @@ namespace LibationWinForms.Dialogs.Login
 {
 	public partial class MfaDialog : Form
 	{
+		private RadioButton[] radioButtons { get; }
+
 		public MfaDialog(AudibleApi.MfaConfig mfaConfig)
 		{
 			InitializeComponent();
+
+			radioButtons = new[] { this.radioButton1, this.radioButton2, this.radioButton3 };
 
 			// optional string settings
 			if (!string.IsNullOrWhiteSpace(mfaConfig.Title))
@@ -37,12 +41,20 @@ namespace LibationWinForms.Dialogs.Login
 			radioButton3.Tag = mfaConfig.Button3Value;
 		}
 
+
 		public string SelectedName { get; private set; }
 		public string SelectedValue { get; private set; }
 		private void submitBtn_Click(object sender, EventArgs e)
 		{
-			var radioButtons = new[] { this.radioButton1, this.radioButton2, this.radioButton3 };
+			Serilog.Log.Logger.Debug("RadioButton states: {@DebugInfo}", new {
+				rb1_checked = radioButton1.Checked,
+				r21_checked = radioButton2.Checked,
+				rb3_checked = radioButton3.Checked
+			});
+
 			var selected = radioButtons.Single(rb => rb.Checked);
+
+			Serilog.Log.Logger.Debug("Selected: {@DebugInfo}", new { isSelected = selected is not null, name = selected?.Name, value = selected?.Tag });
 
 			SelectedName = selected.Name;
 			SelectedValue = (string)selected.Tag;
