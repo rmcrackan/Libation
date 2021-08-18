e36ea70cd1ca0aebd2d4fc5a7d41d3d9e675ede9
Michael Bucari-Tovo <mbucari1@gmail.com>
2021-08-18 15:53:09 -0600

    Removed unnecessary class and simplified.

---
+++ LibationWinForms/DataGridViewImageButtonCell.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace LibationWinForms
+{
+	public class DataGridViewImageButtonCell : DataGridViewButtonCell
+	{
+		protected void DrawButtonImage(Graphics graphics, Image image, Rectangle cellBounds)
+		{
+			var w = image.Width;
+			var h = image.Height;
+			var x = cellBounds.Left + (cellBounds.Width - w) / 2;
+			var y = cellBounds.Top + (cellBounds.Height - h) / 2;
+
+			graphics.DrawImage(image, new Rectangle(x, y, w, h));
+		}
+	}
+}
