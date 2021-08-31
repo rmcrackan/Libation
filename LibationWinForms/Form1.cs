90731a89480c637ec118d7045a04b357b43a283d
Robert McRackan <rmcracka@itron.com>
2021-08-31 09:32:50 -0400

    'Convert all M4b to Mp3': add confirmation dialog with better explanation

--- LibationWinForms/Form1.cs
+++ LibationWinForms/Form1.cs
@@ -389,7 +389,18 @@ namespace LibationWinForms
 			=> await BookLiberation.ProcessorAutomationController.BackupAllPdfsAsync();
 
 		private async void convertAllM4bToMp3ToolStripMenuItem_Click(object sender, EventArgs e)
-			=> await BookLiberation.ProcessorAutomationController.ConvertAllBooksAsync();
+		{
+			var result = MessageBox.Show(
+				"This converts all m4b titles in your library to mp3 files. Original files are not deleted."
+				+ "\r\nFor large libraries this will take a long time and will take up more disk space."
+				+ "\r\n\r\nContinue?"
+				+ "\r\n\r\n(To always download titles as mp3 instead of m4b, go to Settings: Download my books as .MP3 files)",
+				"Convert all M4b => Mp3?",
+				MessageBoxButtons.YesNo,
+				MessageBoxIcon.Warning);
+			if (result == DialogResult.Yes)
+				await BookLiberation.ProcessorAutomationController.ConvertAllBooksAsync();
+		}
 
 		#endregion
 
