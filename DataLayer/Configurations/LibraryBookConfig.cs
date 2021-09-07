c337c0b44eb15fde020c6048cfde9e89c8499602
Robert McRackan <rmcracka@itron.com>
2021-09-07 13:35:59 -0400

    library book composite key comments

--- DataLayer/Configurations/LibraryBookConfig.cs
+++ DataLayer/Configurations/LibraryBookConfig.cs
@@ -7,6 +7,20 @@ namespace DataLayer.Configurations
     {
         public void Configure(EntityTypeBuilder<LibraryBook> entity)
         {
+            // to allow same book (incl region) with diff acct.s:
+            //
+            // this file:
+            // - composite key:
+            //     entity.HasKey(b => new { b.BookId, b.Account });
+            //     entity.HasIndex(b => b.BookId);
+            //     entity.HasIndex(b => b.Account);
+            // - change the below relationship since Book+LibraryBook would no longer be 1:1
+            //
+            // other files:
+            // - change Book class since Book+LibraryBook would no longer be 1:1
+            // - update LibraryBook import code
+            // - would likely challenge assumptions throughout Libation which have been true up until now
+
             entity.HasKey(b => b.BookId);
 
             entity
