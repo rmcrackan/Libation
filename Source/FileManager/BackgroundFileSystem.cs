6411d23744e1c04548c7b5d3dde24b52e4e3b7cd
Michael Bucari-Tovo <mbucari1@gmail.com>
2022-06-23 11:34:20 -0600

    Revert "Non-null disposed BlockingCollection can throw exception"
    This reverts commit ba722487d8c7be977d8246e152131e9c1d829b2f.

--- Source/FileManager/BackgroundFileSystem.cs
+++ Source/FileManager/BackgroundFileSystem.cs
@@ -79,13 +79,8 @@ namespace FileManager
             //Stop raising events
             fileSystemWatcher?.Dispose();
 
-            try
-            {
-                //Calling CompleteAdding() will cause background scanner to terminate.
-                directoryChangesEvents?.CompleteAdding();
-            }
-            // if directoryChangesEvents is non-null and isDisposed, this exception is thrown. there's no other way to check >:(
-            catch (ObjectDisposedException) { }
+            //Calling CompleteAdding() will cause background scanner to terminate.
+            directoryChangesEvents?.CompleteAdding();
 
             //Wait for background scanner to terminate before reinitializing.
             backgroundScanner?.Wait();
