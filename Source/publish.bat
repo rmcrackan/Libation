6826237657899deb6afdd22c69b203ef7656b294
Michael Bucari-Tovo <mbucari1@gmail.com>
2022-06-26 01:16:17 -0600

    Use powershell script to publish and zip libation

--- Source/publish.bat
+++
@@ -1,4 +0,0 @@
-rmdir bin\Publish /S /Q
-dotnet publish -c Release LibationWinForms\LibationWinForms.csproj -p:PublishProfile=LibationWinForms\Properties\PublishProfiles\FolderProfile.pubxml
-dotnet publish -c Release LibationCli\LibationCli.csproj -p:PublishProfile=LibationCli\Properties\PublishProfiles\FolderProfile.pubxml
-dotnet publish -c Release Hangover\Hangover.csproj -p:PublishProfile=Hangover\Properties\PublishProfiles\FolderProfile.pubxml
