17b0da358f516413d9b2e6ca6d4be96bbd58ffef
Michael Bucari-Tovo <mbucari1@gmail.com>
2022-07-22 20:11:13 -0600

    Add LinkLabel control

---
+++ Source/LibationWinForms/AvaloniaUI/Controls/LinkLabel.axaml.cs
@@ -0,0 +1,34 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+using System;
+
+namespace LibationWinForms.AvaloniaUI.Controls
+{
+	public partial class LinkLabel : TextBlock, IStyleable
+	{
+		Type IStyleable.StyleKey => typeof(TextBlock);
+		private static readonly Cursor HandCursor = new Cursor(StandardCursorType.Hand);
+		public LinkLabel()
+		{
+			InitializeComponent();
+		}
+		protected override void OnPointerEnter(PointerEventArgs e)
+		{
+			this.Cursor = HandCursor;
+			base.OnPointerEnter(e);
+		}
+		protected override void OnPointerLeave(PointerEventArgs e)
+		{
+			this.Cursor = Cursor.Default;
+			base.OnPointerLeave(e);
+		}
+
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
+	}
+}
