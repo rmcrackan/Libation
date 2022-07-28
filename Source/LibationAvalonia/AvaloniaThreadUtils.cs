7525d318c0c38a42c7772e0d9cc6e33488ab5cb1
Michael Bucari-Tovo <mbucari1@gmail.com>
2022-07-28 11:03:22 -0600

    Crean up helper methods

--- Source/LibationAvalonia/AvaloniaThreadUtils.cs
+++ Source/LibationAvalonia/AvaloniaThreadUtils.cs
@@ -7,35 +7,25 @@ namespace Avalonia.Threading
 	internal static class AvaloniaThreadUtils
 	{
 		public static TResult Invoke<TResult>(this Dispatcher dispatcher, Func<TResult> function, DispatcherPriority dispatcherPriority = DispatcherPriority.Normal)
-		{
-			using var source = new CancellationTokenSource();
-			var task = dispatcher.InvokeAsync(function, dispatcherPriority);
-			task.ContinueWith(t => source.Cancel(), TaskScheduler.FromCurrentSynchronizationContext());
-			dispatcher.MainLoop(source.Token);
-			return task.Result;
-		}
+			=> WaitOnDispatcherAndGetResult(dispatcher.InvokeAsync(function, dispatcherPriority), dispatcher);
 
 		public static void Invoke(this Dispatcher dispatcher, Action action, DispatcherPriority dispatcherPriority = DispatcherPriority.Normal)
-		{
-			using var source = new CancellationTokenSource();
-			var task = dispatcher.InvokeAsync(action, dispatcherPriority);
-			task.ContinueWith(t => source.Cancel(), TaskScheduler.FromCurrentSynchronizationContext());
-			Dispatcher.UIThread.MainLoop(source.Token);
-		}
+			=> WaitOnDispatcher(dispatcher.InvokeAsync(action, dispatcherPriority), dispatcher);
 
-		public static T WaitOnUIAndGetResult<T>(this Task<T> task)
+		public static TResult WaitOnUIAndGetResult<TResult>(this Task<TResult> task)
 			=> WaitOnDispatcherAndGetResult(task, Dispatcher.UIThread);
-
-		public static T WaitOnDispatcherAndGetResult<T>(this Task<T> task, Dispatcher dispatcher)
-		{
-			using var source = new CancellationTokenSource();
-			task.ContinueWith(t => source.Cancel(), TaskScheduler.FromCurrentSynchronizationContext());
-			dispatcher.MainLoop(source.Token);
-			return task.Result;
-		}
 
 		public static void WaitOnUI(this Task task)
 			=> WaitOnDispatcher(task, Dispatcher.UIThread);
+
+		public static TResult WaitOnDispatcherAndGetResult<TResult>(this Task<TResult> task, Dispatcher dispatcher)
+		{
+			using var source = new CancellationTokenSource();
+			task.ContinueWith(t => source.Cancel(), TaskScheduler.FromCurrentSynchronizationContext());
+			dispatcher.MainLoop(source.Token);
+			return task.Result;
+		}
+
 		public static void WaitOnDispatcher(this Task task, Dispatcher dispatcher)
 		{
 			using var source = new CancellationTokenSource();
