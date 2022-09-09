87db5cfd942e4cd0fec0d9c6ee1921088b4ed452
Robert McRackan <rmcrackan@gmail.com>
2022-09-09 11:04:14 -0400

    revert accidental re-name of button text

--- Source/LibationWinForms/Dialogs/AccountsDialog.Designer.cs
+++ Source/LibationWinForms/Dialogs/AccountsDialog.Designer.cs
@@ -28,134 +28,134 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.cancelBtn = new System.Windows.Forms.Button();
-			this.saveBtn = new System.Windows.Forms.Button();
-			this.dataGridView1 = new System.Windows.Forms.DataGridView();
-			this.DeleteAccount = new System.Windows.Forms.DataGridViewButtonColumn();
-			this.ExportAccount = new System.Windows.Forms.DataGridViewButtonColumn();
-			this.LibraryScan = new System.Windows.Forms.DataGridViewCheckBoxColumn();
-			this.AccountId = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.Locale = new System.Windows.Forms.DataGridViewComboBoxColumn();
-			this.AccountName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.importBtn = new System.Windows.Forms.Button();
-			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// cancelBtn
-			// 
-			this.cancelBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.cancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.cancelBtn.Location = new System.Drawing.Point(832, 479);
-			this.cancelBtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-			this.cancelBtn.Name = "cancelBtn";
-			this.cancelBtn.Size = new System.Drawing.Size(88, 27);
-			this.cancelBtn.TabIndex = 2;
-			this.cancelBtn.Text = "Cancel";
-			this.cancelBtn.UseVisualStyleBackColor = true;
-			this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
-			// 
-			// saveBtn
-			// 
-			this.saveBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.saveBtn.Location = new System.Drawing.Point(714, 479);
-			this.saveBtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-			this.saveBtn.Name = "saveBtn";
-			this.saveBtn.Size = new System.Drawing.Size(88, 27);
-			this.saveBtn.TabIndex = 1;
-			this.saveBtn.Text = "pub ";
-			this.saveBtn.UseVisualStyleBackColor = true;
-			this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
-			// 
-			// dataGridView1
-			// 
-			this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.DeleteAccount = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.ExportAccount = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.LibraryScan = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.AccountId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Locale = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.AccountName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.importBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelBtn.Location = new System.Drawing.Point(832, 479);
+            this.cancelBtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(88, 27);
+            this.cancelBtn.TabIndex = 2;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveBtn.Location = new System.Drawing.Point(714, 479);
+            this.saveBtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(88, 27);
+            this.saveBtn.TabIndex = 1;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
-			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.DeleteAccount,
             this.ExportAccount,
             this.LibraryScan,
             this.AccountId,
             this.Locale,
             this.AccountName});
-			this.dataGridView1.Location = new System.Drawing.Point(14, 14);
-			this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-			this.dataGridView1.MultiSelect = false;
-			this.dataGridView1.Name = "dataGridView1";
-			this.dataGridView1.Size = new System.Drawing.Size(905, 458);
-			this.dataGridView1.TabIndex = 0;
-			this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
-			this.dataGridView1.DefaultValuesNeeded += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridView1_DefaultValuesNeeded);
-			// 
-			// DeleteAccount
-			// 
-			this.DeleteAccount.HeaderText = "Delete";
-			this.DeleteAccount.Name = "DeleteAccount";
-			this.DeleteAccount.ReadOnly = true;
-			this.DeleteAccount.Text = "x";
-			this.DeleteAccount.Width = 46;
-			// 
-			// ExportAccount
-			// 
-			this.ExportAccount.HeaderText = "Export";
-			this.ExportAccount.Name = "ExportAccount";
-			this.ExportAccount.Text = "Export to audible-cli";
-			this.ExportAccount.Width = 47;
-			// 
-			// LibraryScan
-			// 
-			this.LibraryScan.HeaderText = "Include in library scan?";
-			this.LibraryScan.Name = "LibraryScan";
-			this.LibraryScan.Width = 94;
-			// 
-			// AccountId
-			// 
-			this.AccountId.HeaderText = "Audible email/login";
-			this.AccountId.Name = "AccountId";
-			this.AccountId.Width = 125;
-			// 
-			// Locale
-			// 
-			this.Locale.HeaderText = "Locale";
-			this.Locale.Name = "Locale";
-			this.Locale.Width = 47;
-			// 
-			// AccountName
-			// 
-			this.AccountName.HeaderText = "Account nickname (optional)";
-			this.AccountName.Name = "AccountName";
-			this.AccountName.Width = 170;
-			// 
-			// importBtn
-			// 
-			this.importBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-			this.importBtn.Location = new System.Drawing.Point(14, 480);
-			this.importBtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-			this.importBtn.Name = "importBtn";
-			this.importBtn.Size = new System.Drawing.Size(156, 27);
-			this.importBtn.TabIndex = 1;
-			this.importBtn.Text = "Import from audible-cli";
-			this.importBtn.UseVisualStyleBackColor = true;
-			this.importBtn.Click += new System.EventHandler(this.importBtn_Click);
-			// 
-			// AccountsDialog
-			// 
-			this.AcceptButton = this.saveBtn;
-			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.CancelButton = this.cancelBtn;
-			this.ClientSize = new System.Drawing.Size(933, 519);
-			this.Controls.Add(this.dataGridView1);
-			this.Controls.Add(this.importBtn);
-			this.Controls.Add(this.saveBtn);
-			this.Controls.Add(this.cancelBtn);
-			this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-			this.Name = "AccountsDialog";
-			this.Text = "Audible Accounts";
-			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-			this.ResumeLayout(false);
+            this.dataGridView1.Location = new System.Drawing.Point(14, 14);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(905, 458);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
+            this.dataGridView1.DefaultValuesNeeded += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridView1_DefaultValuesNeeded);
+            // 
+            // DeleteAccount
+            // 
+            this.DeleteAccount.HeaderText = "Delete";
+            this.DeleteAccount.Name = "DeleteAccount";
+            this.DeleteAccount.ReadOnly = true;
+            this.DeleteAccount.Text = "x";
+            this.DeleteAccount.Width = 46;
+            // 
+            // ExportAccount
+            // 
+            this.ExportAccount.HeaderText = "Export";
+            this.ExportAccount.Name = "ExportAccount";
+            this.ExportAccount.Text = "Export to audible-cli";
+            this.ExportAccount.Width = 47;
+            // 
+            // LibraryScan
+            // 
+            this.LibraryScan.HeaderText = "Include in library scan?";
+            this.LibraryScan.Name = "LibraryScan";
+            this.LibraryScan.Width = 94;
+            // 
+            // AccountId
+            // 
+            this.AccountId.HeaderText = "Audible email/login";
+            this.AccountId.Name = "AccountId";
+            this.AccountId.Width = 125;
+            // 
+            // Locale
+            // 
+            this.Locale.HeaderText = "Locale";
+            this.Locale.Name = "Locale";
+            this.Locale.Width = 47;
+            // 
+            // AccountName
+            // 
+            this.AccountName.HeaderText = "Account nickname (optional)";
+            this.AccountName.Name = "AccountName";
+            this.AccountName.Width = 170;
+            // 
+            // importBtn
+            // 
+            this.importBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.importBtn.Location = new System.Drawing.Point(14, 480);
+            this.importBtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.importBtn.Name = "importBtn";
+            this.importBtn.Size = new System.Drawing.Size(156, 27);
+            this.importBtn.TabIndex = 1;
+            this.importBtn.Text = "Import from audible-cli";
+            this.importBtn.UseVisualStyleBackColor = true;
+            this.importBtn.Click += new System.EventHandler(this.importBtn_Click);
+            // 
+            // AccountsDialog
+            // 
+            this.AcceptButton = this.saveBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancelBtn;
+            this.ClientSize = new System.Drawing.Size(933, 519);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.importBtn);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.cancelBtn);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "AccountsDialog";
+            this.Text = "Audible Accounts";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
 
 		}
 
