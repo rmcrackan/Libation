378cf7057ee90d10544125a79da57b1b549b6c56
Robert McRackan <rmcrackan@gmail.com>
2023-02-24 13:12:18 -0500

    updated to AudibleApi v8

--- Source/FileLiberator/DownloadOptions.cs
+++ Source/FileLiberator/DownloadOptions.cs
@@ -22,7 +22,7 @@ namespace FileLiberator
 		public OutputFormat OutputFormat { get; init; }
 		public ChapterInfo ChapterInfo { get; init; }
 		public NAudio.Lame.LameConfig LameConfig { get; init; }
-		public string UserAgent => AudibleApi.Resources.USER_AGENT;
+		public string UserAgent => AudibleApi.Resources.User_Agent;
 		public bool TrimOutputToChapterLength => config.AllowLibationFixup && config.StripAudibleBrandAudio;
 		public bool StripUnabridged => config.AllowLibationFixup && config.StripUnabridged;
 		public bool CreateCueSheet => config.CreateCueSheet;
