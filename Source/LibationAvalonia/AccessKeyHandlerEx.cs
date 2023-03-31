7e1ec47b4668b3f8606ea1c7960669a8525e0187
Mbucari <mbucari1@gmail.com>
2023-03-31 11:59:48 -0600

    Tweak AccessKeyHandler

--- Source/LibationAvalonia/AccessKeyHandlerEx.cs
+++ Source/LibationAvalonia/AccessKeyHandlerEx.cs
@@ -11,26 +11,21 @@ namespace LibationAvalonia
 
         public AccessKeyHandlerEx(KeyModifiers menuKeyModifier)
         {
-            KeyModifier = menuKeyModifier;
-            ActivatorKeys = menuKeyModifier switch
-            {
-                KeyModifiers.Alt => new[] { Key.LeftAlt, Key.RightAlt },
-                KeyModifiers.Control => new[] { Key.LeftCtrl, Key.RightCtrl },
-                KeyModifiers.Meta => new[] { Key.LWin, Key.RWin },
-                _ => throw new System.NotSupportedException($"{nameof(KeyModifiers)}.{menuKeyModifier} is not implemented"),
-            };
-        }
+			KeyModifier = menuKeyModifier;
+			ActivatorKeys = menuKeyModifier switch
+			{
+				KeyModifiers.Alt => new[] { Key.LeftAlt, Key.RightAlt },
+				KeyModifiers.Control => new[] { Key.LeftCtrl, Key.RightCtrl },
+				KeyModifiers.Meta => new[] { Key.LWin, Key.RWin },
+				_ => throw new System.NotSupportedException($"{nameof(KeyModifiers)}.{menuKeyModifier} is not implemented"),
+			};
+		}
 
         protected override void OnPreviewKeyDown(object sender, KeyEventArgs e)
         {
-            if (ActivatorKeys.Contains(e.Key))
+			if (ActivatorKeys.Contains(e.Key) && e.KeyModifiers.HasAllFlags(KeyModifier))
             {
-                var newArgs = new KeyEventArgs
-                {
-                    Key = Key.LeftAlt,
-                    Handled = e.Handled,
-                    KeyModifiers = e.KeyModifiers,
-                };
+                var newArgs = new KeyEventArgs { Key = Key.LeftAlt, Handled = e.Handled };
                 base.OnPreviewKeyDown(sender, newArgs);
                 e.Handled = newArgs.Handled;
             }
@@ -38,14 +33,9 @@ namespace LibationAvalonia
 
         protected override void OnPreviewKeyUp(object sender, KeyEventArgs e)
         {
-            if (ActivatorKeys.Contains(e.Key))
+            if (ActivatorKeys.Contains(e.Key) && e.KeyModifiers.HasAllFlags(KeyModifier))
             {
-                var newArgs = new KeyEventArgs()
-                {
-                    Key = Key.LeftAlt,
-                    Handled = e.Handled,
-                    KeyModifiers = e.KeyModifiers,
-                };
+                var newArgs = new KeyEventArgs { Key = Key.LeftAlt,  Handled = e.Handled };
                 base.OnPreviewKeyUp(sender, newArgs);
                 e.Handled = newArgs.Handled;
             }
@@ -55,12 +45,7 @@ namespace LibationAvalonia
         {
             if (e.KeyModifiers.HasAllFlags(KeyModifier))
             {
-                var newArgs = new KeyEventArgs
-                {
-                    Key = e.Key,
-                    Handled = e.Handled,
-                    KeyModifiers = KeyModifiers.Alt,
-                };
+                var newArgs = new KeyEventArgs { Key = e.Key, Handled = e.Handled, KeyModifiers = KeyModifiers.Alt };
                 base.OnKeyDown(sender, newArgs);
                 e.Handled = newArgs.Handled;
             }
