697f7975093a99510272a882fa6b834370c60484
Mbucari <mbucari1@gmail.com>
2023-06-14 11:16:53 -0600

    Remove debug code

--- Source/FileLiberator/DownloadDecryptBook.cs
+++ Source/FileLiberator/DownloadDecryptBook.cs
@@ -396,7 +396,7 @@ namespace FileLiberator
         {
 			if (!Configuration.Instance.DownloadCoverArt) return;
 
-            var coverPath = "[null]";throw new Exception();
+            var coverPath = "[null]";
 
             try
             {
