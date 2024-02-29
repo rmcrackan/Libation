d3f0fd711e54ce82935e4958b7d37be38c3dc4c5
Robert McRackan <rmcrackan@gmail.com>
2024-02-29 14:44:41 -0500

    Remove validation check: SeriesName is null . At least 1 exception found in the wild

--- Source/AudibleUtilities/AudibleApiValidators.cs
+++ Source/AudibleUtilities/AudibleApiValidators.cs
@@ -90,8 +90,10 @@ namespace AudibleUtilities
 			var distinct = items.GetSeriesDistinct();
 			if (distinct.Any(s => s.SeriesId is null))
 				exceptions.Add(new ArgumentException($"Collection contains {nameof(Item.Series)} with null {nameof(Series.SeriesId)}", nameof(items)));
-			if (distinct.Any(s => s.SeriesName is null))
-				exceptions.Add(new ArgumentException($"Collection contains {nameof(Item.Series)} with null {nameof(Series.SeriesName)}", nameof(items)));
+
+			//// unfortunately, a user has a series with no name
+			//if (distinct.Any(s => s.SeriesName is null))
+			//    exceptions.Add(new ArgumentException($"Collection contains {nameof(Item.Series)} with null {nameof(Series.SeriesName)}", nameof(items)));
 
 			return exceptions;
 		}
