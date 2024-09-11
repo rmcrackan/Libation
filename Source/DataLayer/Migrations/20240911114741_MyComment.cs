d1d0a7e4871b4e581a21a81755778d30951e0441
Robert McRackan <rmcrackan@gmail.com>
2024-09-11 07:56:18 -0400

    db migration for IsFinished

---
+++ Source/DataLayer/Migrations/20240911114741_MyComment.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class MyComment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFinished",
+                table: "UserDefinedItem",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFinished",
+                table: "UserDefinedItem");
+        }
+    }
+}
