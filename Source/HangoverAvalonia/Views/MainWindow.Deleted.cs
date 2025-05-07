1a5684799c757fe4c8f5f58ea6bbc6cf80d28150
Michael Bucari-Tovo <mbucari1@gmail.com>
2025-05-07 13:12:29 -0600

    Update Hangover styles and behaviors

--- Source/HangoverAvalonia/Views/MainWindow.Deleted.cs
+++ Source/HangoverAvalonia/Views/MainWindow.Deleted.cs
@@ -1,40 +1,12 @@
-﻿using ApplicationServices;
-using DataLayer;
-using HangoverAvalonia.Controls;
-using System.Linq;
+﻿namespace HangoverAvalonia.Views;
 
-namespace HangoverAvalonia.Views
+public partial class MainWindow
 {
-	public partial class MainWindow
+	private void deletedTab_VisibleChanged(bool isVisible)
 	{
-		private void deletedTab_VisibleChanged(bool isVisible)
-		{
-			if (!isVisible)
-				return;
+		if (!isVisible)
+			return;
 
-			if (_viewModel.DeletedBooks.Count == 0)
-				_viewModel.reload();
-		}
-		public void Deleted_CheckedListBox_ItemCheck(object sender, ItemCheckEventArgs args)
-		{
-			_viewModel.CheckedBooksCount = deletedCbl.CheckedItems.Count();
-		}
-		public void Deleted_CheckAll_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
-		{
-			foreach (var item in deletedCbl.Items)
-				deletedCbl.SetItemChecked(item, true);
-		}
-		public void Deleted_UncheckAll_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
-		{
-			foreach (var item in deletedCbl.Items)
-				deletedCbl.SetItemChecked(item, false);
-		}
-		public void Deleted_Save_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
-		{
-			var libraryBooksToRestore = deletedCbl.CheckedItems.Cast<LibraryBook>().ToList();
-			var qtyChanges = libraryBooksToRestore.RestoreBooks();
-			if (qtyChanges > 0)
-				_viewModel.reload();
-		}
+		_viewModel.TrashBinViewModel.Reload();
 	}
 }
