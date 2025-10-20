07e51f219118d832cfcafe4e417a857b8bc4ef4e
delebash <danelebash@hotmail.com>
2025-10-20 13:09:41 -0400

    IncludedUntil migration

---
+++ Source/DataLayer/Migrations/20251018045524_IncludedUntil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncludedUntil : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "IncludedUntil",
+                table: "LibraryBooks",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IncludedUntil",
+                table: "LibraryBooks");
+        }
+    }
+}
