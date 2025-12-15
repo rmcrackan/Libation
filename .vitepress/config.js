98d3f8557913766c8ea1e4430931f0d15e89e0ba
radiorambo <thisisumesh@proton.me>
2025-12-15 15:28:05 +0530

    install vitepress and configure

---
+++ .vitepress/config.js
@@ -0,0 +1,95 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Libation",
+  description: "Libation: Liberate your Library - A free application for downloading your Audible audiobooks",
+  head: [["link", { rel: "icon", href: "/.vitepress/public/favicon.ico" }]],
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: {
+      light: "/.vitepress/public/libation_logo_light.svg",
+      dark: "/.vitepress/public/libation_logo_dark.svg",
+    },
+
+    footer: {
+      message: "Released under the GPLv3 License",
+    },
+
+    editLink: {
+      pattern: "https://github.com/rmcrackan/Libation/edit/main/:path",
+    },
+
+    lastUpdated: true,
+
+    nav: [
+      { text: "Home", link: "/index.md" },
+      { text: "Documentation", link: "/docs/index" },
+      { text: "FAQ", link: "/docs/frequently-asked-questions" },
+      { text: "Donate", link: "https://www.paypal.com/paypalme/mcrackan" },
+    ],
+    sidebar: [
+      {
+        items: [
+          { text: "Getting Started", link: "/docs/index" },
+          { text: "FAQ", link: "/docs/frequently-asked-questions" },
+          {
+            text: "Report Issues",
+            link: "https://github.com/rmcrackan/Libation/issues",
+          },
+          { text: "Donate", link: "https://www.paypal.com/paypalme/mcrackan" },
+        ],
+      },
+      {
+        text: "Installation",
+        collapsed: false,
+
+        items: [
+          {
+            text: "Install on Linux",
+            link: "/docs/installation/install-on-linux",
+          },
+          { text: "Install on Mac", link: "/docs/installation/install-on-mac" },
+          { text: "Docker", link: "/docs/installation/docker" },
+        ],
+      },
+      {
+        text: "Features",
+        collapsed: false,
+
+        items: [
+          {
+            text: "Audio File Formats",
+            link: "/docs/features/audio-file-formats",
+          },
+          { text: "Naming Templates", link: "/docs/features/naming-templates" },
+          {
+            text: "Searching & Filtering",
+            link: "/docs/features/searching-and-filtering",
+          },
+        ],
+      },
+      {
+        text: "Advanced",
+        collapsed: false,
+        items: [
+          { text: "Advanced Topics", link: "/docs/advanced/advanced" },
+          {
+            text: "Linux Development Setup",
+            link: "/docs/advanced/linux-development-setup-using-nix",
+          },
+        ],
+      },
+    ],
+
+    outline: {
+      level: "deep",
+    },
+
+    socialLinks: [{ icon: "github", link: "https://github.com/rmcrackan/Libation" }],
+
+    search: {
+      provider: "local",
+    },
+  },
+});
