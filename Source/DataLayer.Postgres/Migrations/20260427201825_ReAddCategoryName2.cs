b8616a6da0b9f589e51138b9a6430f40ea1ca204
Michael Bucari-Tovo <mbucari1@gmail.com>
2026-04-27 14:45:13 -0600

    Re-add Category Name Column to database
    Category names were accidently removed during the last DB migration in d67692355f3911ce6722766c6f93a9d47bda4a03.
    
    Re-add the Names column and modify CategoryImporter to upsert the names on update.

---
+++ Source/DataLayer.Postgres/Migrations/20260427201825_ReAddCategoryName2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReAddCategoryName2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Categories");
+        }
+    }
+}
