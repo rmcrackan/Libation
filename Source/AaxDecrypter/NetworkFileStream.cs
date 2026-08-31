4cd784787e370912d9aac8d4f5ed452b33e9fab1
Robert McRackan <rmcrackan@metabolon.com>
2026-08-30 21:40:38 -0400

    avoid infinite retry loops

--- Source/AaxDecrypter/NetworkFileStream.cs
+++ Source/AaxDecrypter/NetworkFileStream.cs
@@ -179,13 +179,17 @@ public class NetworkFileStream : Stream, IUpdatable
 		DownloadTask = Task.Run(() => DownloadLoopInternal(client, response), _cancellationSource.Token);
 	}
 
-	private async Task DownloadLoopInternal(HttpClient client, BlockResponse blockResponse)
+    // avoid infinite retry loops if the server consistently fails for a particular title
+    private const int MAX_TLS_RETRIES = 5;
+
+    private async Task DownloadLoopInternal(HttpClient client, BlockResponse blockResponse)
 	{
 		try
 		{
-			long startPosition = WritePosition;
+			var startPosition = WritePosition;
 
-			while (WritePosition < ContentLength && !IsCancelled)
+            var tlsRetryCount = 0;
+            while (WritePosition < ContentLength && !IsCancelled)
 			{
 				try
 				{
@@ -210,7 +214,8 @@ public class NetworkFileStream : Stream, IUpdatable
                 catch (IOException e)
                     when (e.InnerException is System.ComponentModel.Win32Exception { NativeErrorCode: -2146893008 }
                             && WritePosition != startPosition
-                            && WritePosition < ContentLength && !IsCancelled)
+                            && WritePosition < ContentLength && !IsCancelled
+							&& ++tlsRetryCount <= MAX_TLS_RETRIES)
                 {
                     Serilog.Log.Logger.Warning($"TLS decryption failure at position 0x{WritePosition:X10}. Reconnecting and resuming download.");
 
